c2bea091e3b6de68717996c7cd44c372df7a89e8
akobartek <sokolowskijbartek@gmail.com>
2024-04-25 16:17:47 +0200

    viewmodel tests

--- app/src/main/java/pl/sokolowskibartlomiej/birthdayrecruitment/presentation/utils/ContextUtils.kt
+++ app/src/main/java/pl/sokolowskibartlomiej/birthdayrecruitment/presentation/utils/ContextUtils.kt
@@ -1,7 +1,9 @@
 package pl.sokolowskibartlomiej.birthdayrecruitment.presentation.utils
 
+import android.annotation.SuppressLint
 import android.content.Context
 
+@SuppressLint("DiscouragedApi")
 fun Context.getDrawableId(name:String): Int {
     return this.resources.getIdentifier(name , "drawable", this.packageName)
 }
